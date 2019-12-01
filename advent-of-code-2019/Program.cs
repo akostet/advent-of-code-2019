b2b79e180eb52cc6bfe3c0a68be638872cb667cb
akostet <anton.kostet@gmail.com>
2019-12-01 11:38:59 +0100

    Initial commit

---
+++ advent-of-code-2019/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace advent_of_code_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+    }
+}
